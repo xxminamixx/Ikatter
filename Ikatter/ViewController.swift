41e26c686819bd4fd4e4dc1b58bebd996cc6f4ad
南　京兵 <k_minami@waku-2.com>
2017-04-18 15:05:34 +0900

    最初のコミット

---
+++ Ikatter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ikatter
+//
+//  Created by 南　京兵 on 2017/04/18.
+//  Copyright © 2017年 南　京兵. All rights reserved.
+//
+
+import UIKit
+import Social
+import Swifter
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
