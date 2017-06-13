88d2983a8d150760ca1304d851368a8b858018c5
南 京兵 <k_minai@waku-2.com>
2017-06-13 19:00:17 +0900

    やや修正

--- Ikatter/Controller/TabBarController.swift
+++ Ikatter/Controller/TabBarController.swift
@@ -19,22 +19,6 @@ class TabBarController: UITabBarController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-//    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-//        if viewController is DummyViewController {
-//            // DummyViewControllerはモーダルを出したい特定のタブに紐付けたViewController
-//            if let currentVC = self.selectedViewController{
-//                //表示させるモーダル
-//                let modalViewController: UIViewController = UIViewController()
-//                //わかりやすく背景を赤色に
-//                modalViewController.view.backgroundColor = UIColor.red
-//                currentVC.present(modalViewController, animated: true, completion: nil)
-//            }
-//            return false
-//        }
-//        return true
-//    }
-
 
 }
 
